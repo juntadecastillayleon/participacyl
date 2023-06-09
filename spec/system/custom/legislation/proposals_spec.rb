f5fb571d41a8989b107a0f3b668a797302d30172
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-06-09 16:15:58 +0200

    Do not quote the summary when description is not defined

--- spec/system/custom/legislation/proposals_spec.rb
+++ spec/system/custom/legislation/proposals_spec.rb
@@ -3,11 +3,12 @@ require "sessions_helper"
 
 describe "Legislation Proposals" do
   let(:proposal) { create(:legislation_proposal) }
-  let(:admin) { create(:administrator) }
-  let!(:admin_document) { create(:document, :admin, documentable: proposal, user: admin.user) }
-  let!(:document) { create(:document, documentable: proposal) }
 
   describe "Show" do
+    let(:admin) { create(:administrator) }
+    let!(:admin_document) { create(:document, :admin, documentable: proposal, user: admin.user) }
+    let!(:document) { create(:document, documentable: proposal) }
+
     scenario "Mark documents uploaded by administrators" do
       visit legislation_process_proposal_path(proposal.process, proposal, locale: :es)
 
@@ -16,6 +17,21 @@ describe "Legislation Proposals" do
       end
       within "#document_#{document.id}" do
         expect(page).not_to have_content "Respuesta de la JCYL subido por el Administrador ##{document.user.id}"
+      end
+    end
+
+    describe "Summary" do
+      it "Do not quote summary when description is not defined" do
+        visit legislation_process_proposal_path(proposal.process, proposal)
+
+        expect(page).not_to have_css "blockquote"
+      end
+
+      it "Summary quoted when description is defined" do
+        proposal.update!(description: "Description for proposal")
+        visit legislation_process_proposal_path(proposal.process, proposal)
+
+        expect(page).to have_css "blockquote", count: 1
       end
     end
   end
